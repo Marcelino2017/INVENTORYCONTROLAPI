a69379afd9a5da669f809bf4a26a542d69d4afe2
Marcelino <marcelinomebe@gmail.com>
2025-05-18 15:20:39 -0500

    Añadir modelo Category con atributos fillables y relaciones a productos

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+    
+    public function getRouteKeyName()
+    {
+        return 'name';
+    }
 }
