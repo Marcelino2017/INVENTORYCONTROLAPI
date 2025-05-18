acae620da6c22dee45e3c5b5efe9569a645a5013
Marcelino <marcelinomebe@gmail.com>
2025-05-18 15:16:20 -0500

    Añadir archivo de rutas API y definir las rutas para categorías

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/* Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+ */
+
+
+include_once __DIR__ . '/app/category.php';
+include_once __DIR__ . '/app/product.php';
