1405fe5f3f17505a11a3dc66af468da8aad9b3d6
mmejia <mmejia1@tecbaco.com>
2025-05-19 16:07:05 -0500

    Refactorización de la sintaxis del middleware de rutas en las rutas de autenticación y categoría para mayor coherencia.

--- routes/app/auth.php
+++ routes/app/auth.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
-Route::middleware(['auth:api'])->group([], function () {
+
+Route::middleware(['auth:api'])->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
 });
+
